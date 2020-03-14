72bc08a8b133cc0f6637afa29b2a0147ba6466b1
Maxim Buzdalov <mbuzdalov@gmail.com>
2020-03-14 03:36:16 +0300

    Remove laziness, add unit tests for non-trivial lambdas.

--- src/main/scala/com/github/mbuzdalov/opl/OnePlusLambda.scala
+++ src/main/scala/com/github/mbuzdalov/opl/OnePlusLambda.scala
@@ -1,11 +1,11 @@
 package com.github.mbuzdalov.opl
 
-import scala.Ordering.Double.IeeeOrdering
-
 class OnePlusLambda(n: Int, lambda: Int) {
   private[this] val logChoose = new MathEx.LogChoose(n)
   private[this] val optimalTimeCache, driftMaximizingCache = Array.fill(n)(Double.NaN)
   private[this] val optimalByStrength, driftMaximizingByStrength, driftByStrength = Array.fill(n, n)(Double.NaN)
+
+  computeEverything()
 
   private def multiplyInPlace(a: Array[Double], b: Array[Double]): Unit = {
     var aa, bb, sum = 0.0
@@ -17,11 +17,14 @@ class OnePlusLambda(n: Int, lambda: Int) {
       sum += a(i)
       i += 1
     }
-    i = 0
-    while (i < a.length) {
-      a(i) /= sum
-      i += 1
-    }
+//    if (sum > 1e-9) {
+//      sum /= 1 - (1 - aa) * (1 - bb)
+//      i = 0
+//      while (i < a.length) {
+//        a(i) /= sum
+//        i += 1
+//      }
+//    }
   }
 
   private def multiplyByPower(power: Int, unit: Array[Double], result: Array[Double]): Unit = {
@@ -34,66 +37,60 @@ class OnePlusLambda(n: Int, lambda: Int) {
     if (p == 1) multiplyInPlace(result, unit)
   }
 
-  def getOptimalTime(d: Int): Double = {
-    if (d == 0)
-      0.0
-    else {
-      if (optimalTimeCache(d - 1).isNaN)
-        optimalTimeCache(d - 1) = (1 to n).view.map(l => getOptimalTime(d, l)).min
-      optimalTimeCache(d - 1)
-    }
-  }
+  def optimalTime(d: Int): Double = if (d == 0) 0.0 else optimalTimeCache(d - 1)
 
   def optimalExpectedTime: Double = {
     val logAll = math.log(2) * n
-    (0 to n).map(d => getOptimalTime(d) * math.exp(logChoose(n, d) - logAll)).sum
+    (0 to n).map(d => optimalTime(d) * math.exp(logChoose(n, d) - logAll)).sum
   }
 
-  def getDriftOptimalTime(d: Int): Double = {
-    if (d == 0)
-      0.0
-    else {
-      if (driftMaximizingCache(d - 1).isNaN) {
-        val bestDrift = (1 to n).maxBy(l => getDrift(d, l))
-        driftMaximizingCache(d - 1) = getDriftOptimalTime(d, bestDrift)
-      }
-      driftMaximizingCache(d - 1)
-    }
-  }
+  def driftOptimalTime(d: Int): Double = if (d == 0) 0.0 else driftMaximizingCache(d - 1)
 
   def driftOptimalExpectedTime: Double = {
     val logAll = math.log(2) * n
-    (0 to n).map(d => getDriftOptimalTime(d) * math.exp(logChoose(n, d) - logAll)).sum
+    (0 to n).map(d => driftOptimalTime(d) * math.exp(logChoose(n, d) - logAll)).sum
   }
 
-  def getOptimalTime(d: Int, l: Int): Double = {
+  def optimalTime(d: Int, l: Int): Double =
     if (d == 0)
       0.0
     else if (l == 0)
       Double.PositiveInfinity
-    else {
-      if (optimalByStrength(d - 1)(l - 1).isNaN) compute(d, l)
+    else
       optimalByStrength(d - 1)(l - 1)
-    }
-  }
 
-  def getDrift(d: Int, l: Int): Double = {
+  def drift(d: Int, l: Int): Double =
     if (d == 0 || l == 0)
       0.0
-    else {
-      if (driftByStrength(d - 1)(l - 1).isNaN) compute(d, l)
+    else
       driftByStrength(d - 1)(l - 1)
-    }
-  }
 
-  def getDriftOptimalTime(d: Int, l: Int): Double = {
+  def driftOptimalTime(d: Int, l: Int): Double =
     if (d == 0)
       0.0
     else if (l == 0)
       Double.PositiveInfinity
-    else {
-      if (driftMaximizingByStrength(d - 1)(l - 1).isNaN) compute(d, l)
+    else
       driftMaximizingByStrength(d - 1)(l - 1)
+
+  private def computeEverything(): Unit = {
+    for (d <- 1 to n) {
+      var bestDriftValue = -1.0
+      var bestDriftIndex = -1
+      var minOptimal = Double.PositiveInfinity
+      var change = 1
+      while (change <= n) {
+        compute(d, change)
+        minOptimal = math.min(minOptimal, optimalByStrength(d - 1)(change - 1))
+        val driftValue = driftByStrength(d - 1)(change - 1)
+        if (bestDriftValue < driftValue) {
+          bestDriftValue = driftValue
+          bestDriftIndex = change
+        }
+        change += 1
+      }
+      optimalTimeCache(d - 1) = minOptimal
+      driftMaximizingCache(d - 1) = driftMaximizingByStrength(d - 1)(bestDriftIndex - 1)
     }
   }
 
@@ -106,32 +103,30 @@ class OnePlusLambda(n: Int, lambda: Int) {
     multiplyByPower(lambda - 1, prob.clone(), prob)
 
     var updateSumOptimal, updateSumDriftOptimal, drift = 0.0
-    var updateProb, stayProb = 0.0
+    var updateProb = 0.0
     var i = 0
     while (i < prob.length) {
       val okay = lower + i
       val newD = d - 2 * okay + change
       val pi = prob(i)
-      if (newD >= d) {
-        stayProb += pi
-      } else {
+      if (newD < d) {
         updateProb += pi
-        updateSumOptimal += getOptimalTime(newD) * pi
-        updateSumDriftOptimal += getDriftOptimalTime(newD) * pi
+        if (newD > 0) {
+          updateSumOptimal += optimalTimeCache(newD - 1) * pi
+          updateSumDriftOptimal += driftMaximizingCache(newD - 1) * pi
+        }
         drift += (d - newD) * pi
       }
       i += 1
     }
 
-    assert(math.abs(updateProb + stayProb - 1) < 1e-9)
-
-    if (stayProb > 1 - 1e-9) {
+    if (updateProb < 1e-9) {
       optimalByStrength(d - 1)(change - 1) = Double.PositiveInfinity
       driftMaximizingByStrength(d - 1)(change - 1) = Double.PositiveInfinity
       driftByStrength(d - 1)(change - 1) = 0
     } else {
-      optimalByStrength(d - 1)(change - 1) = (1 + updateSumOptimal) / (1 - stayProb)
-      driftMaximizingByStrength(d - 1)(change - 1) = (1 + updateSumDriftOptimal) / (1 - stayProb)
+      optimalByStrength(d - 1)(change - 1) = (1 + updateSumOptimal) / updateProb
+      driftMaximizingByStrength(d - 1)(change - 1) = (1 + updateSumDriftOptimal) / updateProb
       driftByStrength(d - 1)(change - 1) = drift
     }
   }
--- src/test/scala/com/github/mbuzdalov/opl/OnePlusLambdaTests.scala
+++ src/test/scala/com/github/mbuzdalov/opl/OnePlusLambdaTests.scala
@@ -4,28 +4,34 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class OnePlusLambdaTests extends AnyFlatSpec with Matchers {
-  private def optimalTimes(n: Int): (Double, Double) = {
-    val opl = new OnePlusLambda(n, 1)
+  private def optimalTimes(n: Int, lambda: Int): (Double, Double) = {
+    val opl = new OnePlusLambda(n, lambda)
     val optimalTime = opl.optimalExpectedTime
     val driftOptimalTime = opl.driftOptimalExpectedTime
     (optimalTime, driftOptimalTime)
   }
 
   "RLS time" should "be right for n=500" in {
-    val (optimalTime, driftOptimalTime) = optimalTimes(500)
+    val (optimalTime, driftOptimalTime) = optimalTimes(500, 1)
     optimalTime should be (2974.0 +- 0.05)
     driftOptimalTime should be (2974.3 +- 0.05)
   }
 
   it should "be right for n=1000" in {
-    val (optimalTime, driftOptimalTime) = optimalTimes(1000)
+    val (optimalTime, driftOptimalTime) = optimalTimes(1000, 1)
     optimalTime should be (6644.0 +- 0.05)
     driftOptimalTime should be (6644.2 +- 0.05)
   }
 
   it should "be right for n=1500" in {
-    val (optimalTime, driftOptimalTime) = optimalTimes(1500)
+    val (optimalTime, driftOptimalTime) = optimalTimes(1500, 1)
     optimalTime should be (10575.7 +- 0.05)
     driftOptimalTime should be (10575.9 +- 0.05)
+  }
+
+  "(1+100) RLS time" should "be right for n=500" in {
+    val (optimalTime, driftOptimalTime) = optimalTimes(500, 100)
+    optimalTime should be (119.93759945813207 +- 1e-6)
+    driftOptimalTime should be (119.94149522047802 +- 1e-6)
   }
 }
