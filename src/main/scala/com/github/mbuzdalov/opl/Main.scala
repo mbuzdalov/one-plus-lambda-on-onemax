ed14e6c7640a8224444edf2d3eda664aebb443cd
Maxim Buzdalov <mbuzdalov@gmail.com>
2020-04-09 01:45:18 +0300

    Implement a runner that saves best parameters.

--- src/main/scala/com/github/mbuzdalov/opl/Main.scala
+++ src/main/scala/com/github/mbuzdalov/opl/Main.scala
@@ -1,13 +1,83 @@
 package com.github.mbuzdalov.opl
 
 import java.nio.file.{Files, Paths}
+import java.util.{ArrayList => JArrayList}
+import java.util.concurrent.{Callable, Executors, TimeUnit}
 
+import scala.jdk.CollectionConverters._
 import scala.util.Using
 
 import com.github.mbuzdalov.opl.computation.{DriftOptimalRunningTime, OptimalMutationRunningTime, OptimalRunningTime}
 import com.github.mbuzdalov.opl.picture.RelativeOptimalityPictureBuilder
 
 object Main {
+  def optimalParameterValues(args: Array[String]): Unit = {
+    val targetRoot = Paths.get(args(0))
+    Files.createDirectories(targetRoot)
+    val nValues = args(1).split(',').map(_.toInt)
+    val lambdaValues = args(2).split(',').map(_.toInt)
+    Using.resources(Files.newBufferedWriter(targetRoot.resolve("optimal-flips.csv")),
+                    Files.newBufferedWriter(targetRoot.resolve("drift-optimal-flips.csv")),
+                    Files.newBufferedWriter(targetRoot.resolve("optimal-mutation-rate-std.csv")),
+                    Files.newBufferedWriter(targetRoot.resolve("optimal-mutation-rate-shift.csv"))) { (opt, drift, std, shf) =>
+      opt.write("n,lambda,distance,value\n")
+      drift.write("n,lambda,distance,value\n")
+      std.write("n,lambda,distance,value\n")
+      shf.write("n,lambda,distance,value\n")
+      Using.resource(Files.newBufferedWriter(targetRoot.resolve("strangeness.txt"))) { strange =>
+        val theLock = new AnyRef
+
+        class Task(n: Int, lambda: Int) extends Callable[Unit] {
+          override def call(): Unit = {
+            val t0 = timer(s"[n=$n, lambda=$lambda] Creating listeners...")
+            val optimalListener = OptimalRunningTime.newListener
+            val driftOptimalListener = DriftOptimalRunningTime.newListener
+            val standardOptimalListener = OptimalMutationRunningTime.newListener(false)
+            val shiftOptimalListener = OptimalMutationRunningTime.newListener(true)
+            t0.done()
+
+            val t1 = timer(s"[n=$n, lambda=$lambda] Running (1+lambda)...")
+            OnePlusLambda(n, lambda, Seq(optimalListener, driftOptimalListener, standardOptimalListener, shiftOptimalListener))
+            val optimalResult = optimalListener.toResult
+            val driftOptimalResult = driftOptimalListener.toResult
+            val standardOptimalResult = standardOptimalListener.toResult
+            val shiftOptimalResult = shiftOptimalListener.toResult
+            t1.done()
+
+            var prevOptP = -1
+            for (distance <- 1 to n) {
+              val optP = optimalResult.optimalParameter(distance)
+              if (optP < prevOptP) {
+                val prevP = optimalResult.optimalExpectationForParameter(distance - 1, prevOptP)
+                val prevC = optimalResult.optimalExpectationForParameter(distance - 1, optP)
+                val currP = optimalResult.optimalExpectationForParameter(distance, prevOptP)
+                val currC = optimalResult.optimalExpectationForParameter(distance, optP)
+                theLock.synchronized {
+                  strange.write(s"n=$n, lambda=$lambda: ${distance - 1}: $prevOptP => $prevP, $optP => $prevC. $distance: $prevOptP => $currP, $optP => $currC\n")
+                }
+              }
+              prevOptP = optP
+              theLock.synchronized {
+                opt.write(s"$n,$lambda,$distance,$optP\n")
+                drift.write(s"$n,$lambda,$distance,${driftOptimalResult.optimalParameter(distance)}\n")
+                std.write(s"$n,$lambda,$distance,${standardOptimalResult.optimalParameter(distance)}\n")
+                shf.write(s"$n,$lambda,$distance,${shiftOptimalResult.optimalParameter(distance)}\n")
+              }
+            }
+          }
+        }
+
+        val tasks = new JArrayList[Task]()
+        for (n <- nValues; lambda <- lambdaValues) tasks.add(new Task(n, lambda))
+        val pool = Executors.newFixedThreadPool(Runtime.getRuntime.availableProcessors())
+        val futures = pool.invokeAll(tasks)
+        futures.asScala.foreach(_.get())
+        pool.shutdown()
+        pool.awaitTermination(1, TimeUnit.HOURS)
+      }
+    }
+  }
+
   def optimalValuesComparison(args: Array[String]): Unit = {
     val targetRoot = Paths.get(args(0))
     Files.createDirectories(targetRoot)
@@ -101,14 +171,16 @@ object Main {
   def main(args: Array[String]): Unit = args(0) match {
     case "pictures" => pictures(args.tail)
     case "optimal-comparison" => optimalValuesComparison(args.tail)
+    case "optimal-parameters" => optimalParameterValues(args.tail)
   }
 
   case class timer(message: String) {
     private val t0 = System.nanoTime()
-    print(message)
 
     def done(): Unit = {
-      println(f" done in ${(System.nanoTime() - t0) * 1e-9}%.3f s")
+      Main.synchronized {
+        println(f"$message done in ${(System.nanoTime() - t0) * 1e-9}%.3f s")
+      }
     }
   }
 }
