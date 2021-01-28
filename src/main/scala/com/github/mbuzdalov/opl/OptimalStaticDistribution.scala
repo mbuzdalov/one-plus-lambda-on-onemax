2ace2fed46d020ab0277e5b70c3bde074658e3af
Maxim Buzdalov <mbuzdalov@gmail.com>
2021-01-28 05:38:17 +0300

    Implement parallel evaluation of static distributions.

--- src/main/scala/com/github/mbuzdalov/opl/OptimalStaticDistribution.scala
+++ src/main/scala/com/github/mbuzdalov/opl/OptimalStaticDistribution.scala
@@ -1,18 +1,72 @@
 package com.github.mbuzdalov.opl
 
-import com.github.mbuzdalov.opl.computation.OptimalRunningTime
-import com.github.mbuzdalov.opl.distribution.ParameterizedDistribution
+import java.io.PrintWriter
+import java.util.concurrent.locks.ReentrantLock
+import java.util.concurrent.{Callable, ConcurrentLinkedDeque, CountDownLatch, Executors}
+
+import scala.annotation.tailrec
+import scala.util.Using
+
 import org.apache.commons.math3.analysis.MultivariateFunction
 import org.apache.commons.math3.optim.nonlinear.scalar.{GoalType, ObjectiveFunction}
 import org.apache.commons.math3.optim.{ConvergenceChecker, InitialGuess, MaxEval, PointValuePair, SimpleBounds}
 import org.apache.commons.math3.optim.nonlinear.scalar.noderiv.CMAESOptimizer
 import org.apache.commons.math3.random.{MersenneTwister, RandomGenerator}
 
-import java.io.PrintWriter
-import scala.util.Using
+import com.github.mbuzdalov.opl.computation.{BareComputationListener, OptimalRunningTime}
+import com.github.mbuzdalov.opl.distribution.ParameterizedDistribution
 
 object OptimalStaticDistribution {
-  class FixedDistribution(distribution: Array[Double]) extends ParameterizedDistribution[Unit] {
+  private class LatchWrapper(val latch: CountDownLatch, var done: Boolean) {
+    def markDone(): Unit = {
+      done = true
+      latch.countDown()
+    }
+  }
+
+  private class OnePlusLambdaServer(n: Int, lambda: Int) {
+    private val entryLock = new ReentrantLock()
+    private val sequence = new ConcurrentLinkedDeque[(BareComputationListener, LatchWrapper)]()
+
+    @tailrec
+    final def compute(listener: BareComputationListener): Unit = {
+      if (entryLock.tryLock()) {
+        val sequenceToEvaluate = IndexedSeq.newBuilder[BareComputationListener]
+        val sequenceToNotify = IndexedSeq.newBuilder[LatchWrapper]
+        sequenceToEvaluate += listener
+        var nextPair = sequence.poll()
+        while (nextPair != null) {
+          sequenceToEvaluate += nextPair._1
+          sequenceToNotify += nextPair._2
+          nextPair = sequence.poll()
+        }
+        val theSequence = sequenceToEvaluate.result()
+        OnePlusLambda(n, lambda, theSequence, printTimings = false)
+        sequenceToNotify.result().foreach(_.markDone())
+        entryLock.unlock()
+        val late = sequence.poll()
+        if (late != null) {
+          late._2.latch.countDown() // as we polled it, it is not done, so give it a chance to work on its own
+        }
+      } else {
+        val myLatch = new LatchWrapper(new CountDownLatch(1), false)
+        sequence.add((listener, myLatch))
+        myLatch.latch.await()
+        if (!myLatch.done) compute(listener)
+      }
+    }
+  }
+
+  private val servers = new scala.collection.mutable.HashMap[(Int, Int), OnePlusLambdaServer]
+  private def runOnServer(n: Int, lambda: Int, listener: BareComputationListener): Unit = {
+    val myServer = servers.synchronized {
+      val key = (n, lambda)
+      servers.getOrElseUpdate(key, new OnePlusLambdaServer(n, lambda))
+    }
+    myServer.compute(listener)
+  }
+
+  private class FixedDistribution(distribution: Array[Double]) extends ParameterizedDistribution[Unit] {
     override def minimize(n: Int, fun: Unit => Double): (Unit, Double) = {} -> fun({})
     override def initialize(n: Int, param: Unit, target: DoubleProbabilityVector): Unit = {
       assert(n == distribution.length)
@@ -26,7 +80,7 @@ object OptimalStaticDistribution {
     }
   }
 
-  class FitnessFunction(lambda: Int) extends MultivariateFunction {
+  private class FitnessFunction(lambda: Int) extends MultivariateFunction {
     private[this] var nCalls = 0
     private[this] var bestFitness = Double.PositiveInfinity
     private[this] var lastUpdate = 0
@@ -35,7 +89,7 @@ object OptimalStaticDistribution {
     override def value(distribution: Array[Double]): Double = {
       val n = distribution.length
       val timeForDistributionListener = OptimalRunningTime.newListener(new FixedDistribution(distribution))
-      OnePlusLambda(n, lambda, Seq(timeForDistributionListener), printTimings = false)
+      runOnServer(n, lambda, timeForDistributionListener)
       val result = timeForDistributionListener.toResult.expectedRunningTime
       nCalls += 1
       if (bestFitness > result) {
@@ -52,27 +106,11 @@ object OptimalStaticDistribution {
     def sequence: IndexedSeq[(Int, Double)] = fitnessSequence.result()
   }
 
-  object NeverConverged extends ConvergenceChecker[PointValuePair] {
+  private object NeverConverged extends ConvergenceChecker[PointValuePair] {
     override def converged(iteration: Int, previous: PointValuePair, current: PointValuePair): Boolean = false
   }
 
-  def makeShiftDistribution(n: Int): Array[Double] = {
-    import MathEx.{logFactorial => lF}
-    val param = 1.0 / n
-    val common = lF(n)
-    val lP = math.log(param)
-    val l1P = math.log1p(-param)
-    val result = new Array[Double](n)
-    result(0) = math.exp(l1P * n) + math.exp(lP + l1P * (n - 1) + common - lF(1) - lF(n - 1))
-    var i = 2
-    while (i <= n) {
-      result(i - 1) = math.exp(lP * i + l1P * (n - i) + common - lF(i) - lF(n - i))
-      i += 1
-    }
-    result
-  }
-
-  def normalize(a: Array[Double]): Unit = {
+  private def normalize(a: Array[Double]): Unit = {
     val sum = a.sum
     var i = a.length
     while (i > 0) {
@@ -100,23 +138,37 @@ object OptimalStaticDistribution {
     RunResult(result.getValue, finalDistribution, objectiveFunction.sequence)
   }
 
+  private val rng = ThreadLocal.withInitial(() => new MersenneTwister())
+  private class Task(n: Int, lambda: Int) extends Callable[RunResult] {
+    override def call(): RunResult = {
+      val start = System.nanoTime()
+      val result = findOptimalDistribution(n, lambda, rng.get())
+      val time = System.nanoTime() - start
+      println(s"  Fitness: ${result.fitness} in ${time * 1e-9} seconds")
+      result
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     val n = args(0).toInt
-    val rng = new MersenneTwister()
 
-    Using.resources(new PrintWriter("static-fitness-log.csv"), new PrintWriter("static-distributions.csv")) { (fitnessLog, distLog) =>
+    val executor = Executors.newFixedThreadPool(50)
+
+    Using.resources(
+      new PrintWriter(s"static-$n-fitness-log.csv"),
+      new PrintWriter(s"static-$n-distributions.csv"),
+      new PrintWriter(s"static-$n-summary.csv")
+    ) { (fitnessLog, distLog, summary) =>
       val globalStartTime = System.nanoTime()
       fitnessLog.println("n,lambda,attempt,evaluation,fitness")
       distLog.println("n,lambda,attempt,distance,probability")
+      summary.println("n,lambda,expectation")
       val finalResults = for (lambda <- (1 to 7) ++ Seq(8, 16, 32, 64, 128, 256, 512, 1024)) yield {
         println("Lambda: " + lambda)
-        val finalFitnessValues = for (_ <- 0 until 50) yield {
-          val start = System.nanoTime()
-          val result = findOptimalDistribution(n, lambda, rng)
-          val time = System.nanoTime() - start
-          println(s"  Fitness: ${result.fitness} in ${time * 1e-9} seconds")
-          result
-        }
+        val queue = new java.util.ArrayList[Task]
+        for (_ <- 0 until 50) queue.add(new Task(n, lambda))
+        val futures = executor.invokeAll(queue)
+        val finalFitnessValues = IndexedSeq.tabulate(futures.size())(i => futures.get(i).get())
         val min = finalFitnessValues.minBy(_.fitness).fitness
         val max = finalFitnessValues.maxBy(_.fitness).fitness
         val similar = finalFitnessValues.filter(_.fitness < min * (1 + 1e-9))
@@ -128,11 +180,14 @@ object OptimalStaticDistribution {
               distLog.println(s"$n,$lambda,$attempt,$i,${distribution(i - 1)}")
 
         }
+        summary.println(s"$n,$lambda,$min")
         s"lambda = $lambda: min = $min, #similar = ${similar.size}, max = $max"
       }
       finalResults.foreach(println)
       val globalTime = System.nanoTime() - globalStartTime
       println(s"Total time spent: ${globalTime * 1e-9} seconds")
     }
+
+    executor.shutdown()
   }
 }
