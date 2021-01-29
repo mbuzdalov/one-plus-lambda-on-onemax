307f74ba366ede8f82838a3df1e695d7564e272e
Maxim Buzdalov <mbuzdalov@gmail.com>
2021-01-29 21:02:34 +0300

    Stabilize the unit test further.

--- src/test/scala/com/github/mbuzdalov/opl/OptimalStaticDistributionTests.scala
+++ src/test/scala/com/github/mbuzdalov/opl/OptimalStaticDistributionTests.scala
@@ -15,7 +15,10 @@ class OptimalStaticDistributionTests extends AnyFlatSpec with Matchers {
   }
 
   it should "be found correctly for n=10, lambda=4" in {
-    val result = OptimalStaticDistribution.findOptimalDistribution(10, 4, new MersenneTwister())
+    val result = (0 until 5).map{ _ =>
+      OptimalStaticDistribution.findOptimalDistribution(10, 4, new MersenneTwister())
+    }.minBy(_.fitness)
+
     result.fitness shouldBe (8.06115492800143 +- 1e-9)
     result.distribution(0) shouldBe (0.9828011749325332 +- 1e-8)
     result.distribution(9) shouldBe (0.01719882506746691 +- 1e-8)
