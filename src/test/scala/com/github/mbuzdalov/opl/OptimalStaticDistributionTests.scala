91b9aba56fce4aba692d6e0876000e74bd4136f3
Maxim Buzdalov <mbuzdalov@gmail.com>
2021-01-30 04:23:49 +0300

    Make static distribution tests harder regarding expectations and test sets.

--- src/test/scala/com/github/mbuzdalov/opl/OptimalStaticDistributionTests.scala
+++ src/test/scala/com/github/mbuzdalov/opl/OptimalStaticDistributionTests.scala
@@ -7,7 +7,7 @@ import org.scalatest.matchers.should.Matchers
 class OptimalStaticDistributionTests extends AnyFlatSpec with Matchers {
   "Optimal static distribution" should "be found correctly for n=10, lambda=1" in {
     val result = OptimalStaticDistribution.findOptimalDistribution(10, 1, new MersenneTwister())
-    result.fitness shouldBe (22.359033978174686 +- 1e-9)
+    result.fitness shouldBe (22.359033978174686 +- 1e-12)
     result.distribution(0) shouldBe (1.0 +- 1e-15)
     for (i <- 1 until 9) {
       result.distribution(i) shouldBe (0.0 +- 1e-15)
@@ -19,11 +19,19 @@ class OptimalStaticDistributionTests extends AnyFlatSpec with Matchers {
       OptimalStaticDistribution.findOptimalDistribution(10, 4, new MersenneTwister())
     }.minBy(_.fitness)
 
-    result.fitness shouldBe (8.06115492800143 +- 1e-9)
+    result.fitness shouldBe (8.06115492800143 +- 1e-12)
     result.distribution(0) shouldBe (0.9828011749325332 +- 1e-8)
     result.distribution(9) shouldBe (0.01719882506746691 +- 1e-8)
     for (i <- 1 until 8) {
       result.distribution(i) shouldBe (0.0 +- 1e-15)
     }
+  }
+
+  it should "be found correctly for n=20, lambda=256" in {
+    val result = (0 until 10).map{ _ =>
+      OptimalStaticDistribution.findOptimalDistribution(20, 256, new MersenneTwister())
+    }.minBy(_.fitness)
+
+    result.fitness shouldBe (3.8274875626092526 +- 1e-12)
   }
 }
