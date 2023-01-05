283eedec545e2a0edc426b64d8ebf438ea4cd628
Maxim Buzdalov <mbuzdalov@gmail.com>
2023-01-05 11:26:15 +0000

    Document the size parameter, rename few private vars as they are now really private.

--- src/main/scala/com/github/mbuzdalov/oll/InMemoryCostPrioritizingCrossoverCache.scala
+++ src/main/scala/com/github/mbuzdalov/oll/InMemoryCostPrioritizingCrossoverCache.scala
@@ -7,30 +7,30 @@ import scala.annotation.tailrec
  * When some entries need to be discarded, those with larger goodBitsInDifference are retained, because they are
  * harder to recompute from scratch.
  *
+ * @param maxCacheByteSize the (approximate) maximum size of all the cache entries, in bytes.
  * @param delegate the crossover computation class used to perform the math.
  */
 class InMemoryCostPrioritizingCrossoverCache(maxCacheByteSize: Long, delegate: CrossoverComputation) extends CrossoverComputation {
   import InMemoryCostPrioritizingCrossoverCache.CacheEntry
 
-  private val cacheEntryOrdering: Ordering[CacheEntry] =
-    (x: CacheEntry, y: CacheEntry) => -java.lang.Long.compare(x.g, y.g)
-  private val probOfReachingFCache = new scala.collection.mutable.HashMap[CacheEntry, CacheEntry]
-  private val cacheEntryQueue = new scala.collection.mutable.PriorityQueue[CacheEntry]()(cacheEntryOrdering)
-  private var cacheByteSize, queries, hits, hitTime, totalHits, misses, missTime, totalMisses = 0L
+  private val entryOrdering: Ordering[CacheEntry] = (x: CacheEntry, y: CacheEntry) => -java.lang.Long.compare(x.g, y.g)
+  private val cache = new scala.collection.mutable.HashMap[CacheEntry, CacheEntry]
+  private val queue = new scala.collection.mutable.PriorityQueue[CacheEntry]()(entryOrdering)
+  private var byteSize, queries, hits, hitTime, totalHits, misses, missTime, totalMisses = 0L
 
   @tailrec
   private def tryAddEntry(e: CacheEntry): Unit = {
-    val newCacheByteSize = cacheByteSize + e.byteSize
+    val newCacheByteSize = byteSize + e.byteSize
     if (newCacheByteSize <= maxCacheByteSize) {
-      probOfReachingFCache.put(e, e)
-      cacheEntryQueue.addOne(e)
-      cacheByteSize = newCacheByteSize
+      cache.put(e, e)
+      queue.addOne(e)
+      byteSize = newCacheByteSize
     } else {
-      val queueTop = cacheEntryQueue.head
+      val queueTop = queue.head
       if (e.g > queueTop.g) {
-        cacheEntryQueue.dequeue()
-        probOfReachingFCache.remove(queueTop)
-        cacheByteSize -= queueTop.byteSize
+        queue.dequeue()
+        cache.remove(queueTop)
+        byteSize -= queueTop.byteSize
         tryAddEntry(e)
       }
     }
@@ -38,14 +38,14 @@ class InMemoryCostPrioritizingCrossoverCache(maxCacheByteSize: Long, delegate: C
 
   override def compute(distanceToParent: Int, goodBitsInDifference: Int, populationSize: Int, crossoverBias: Double): Array[Double] = {
     val entry = CacheEntry(distanceToParent, goodBitsInDifference, populationSize, crossoverBias)
-    probOfReachingFCache.synchronized {
-      assert(probOfReachingFCache.size == cacheEntryQueue.size)
+    cache.synchronized {
+      assert(cache.size == queue.size)
 
       val timeCost = goodBitsInDifference.toLong * goodBitsInDifference
-      val realEntry = if (probOfReachingFCache.contains(entry)) {
+      val realEntry = if (cache.contains(entry)) {
         hits += 1
         hitTime += timeCost
-        probOfReachingFCache(entry)
+        cache(entry)
       } else {
         misses += 1
         missTime += timeCost
@@ -56,7 +56,7 @@ class InMemoryCostPrioritizingCrossoverCache(maxCacheByteSize: Long, delegate: C
       if (queries % 1000000 == 0) {
         totalHits += hits
         totalMisses += misses
-        println(s"[$queries queries, $totalHits hits ($hits new, cost $hitTime), $totalMisses misses ($misses new, cost $missTime), cache size ${probOfReachingFCache.size}, $cacheByteSize bytes in arrays]")
+        println(s"[$queries queries, $totalHits hits ($hits new, cost $hitTime), $totalMisses misses ($misses new, cost $missTime), cache size ${cache.size}, $byteSize bytes in arrays]")
         hits = 0
         misses = 0
         hitTime = 0
@@ -67,7 +67,7 @@ class InMemoryCostPrioritizingCrossoverCache(maxCacheByteSize: Long, delegate: C
   }
 
   override def clear(): Unit = {
-    probOfReachingFCache.clear()
+    cache.clear()
   }
 }
 
