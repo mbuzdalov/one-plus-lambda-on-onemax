61205a81df554e2c2e18ec5f005da3240eee4a34
Maxim Buzdalov <mbuzdalov@gmail.com>
2023-02-09 03:21:19 +0000

    Implement a two-dimensional landscape plotter.

--- src/main/scala/com/github/mbuzdalov/oll/DemoPlots.scala
+++ src/main/scala/com/github/mbuzdalov/oll/DemoPlots.scala
@@ -1,14 +1,80 @@
 package com.github.mbuzdalov.oll
 
+import java.awt.image.BufferedImage
 import java.io.PrintWriter
+import java.nio.file.{Files, Paths}
 import java.util.concurrent.{Callable, ScheduledThreadPoolExecutor}
+
+import javax.imageio.ImageIO
 
 import scala.jdk.CollectionConverters._
 import scala.util.Using
 
 import com.github.mbuzdalov.oll.xover.LegacyCollectiveCrossoverComputation
+import com.github.mbuzdalov.util.Viridis
 
 object DemoPlots {
+  private def varyingLastTwoLambdas(): Unit = {
+    val n = 500
+    val bins = RunGivenLambdas.defaultBins(n)
+    val lambdas = Array(1.0, 1.0, 1.0, 1.0, 6.5, 8.5, 11.5, 16.5, 24.5)
+
+    val crossoverComputation = new InMemoryCostPrioritizingCrossoverCache(
+      maxCacheByteSize = 4000000000L, //cmd.getLong("max-cache-byte-size"),
+      delegate = LegacyCollectiveCrossoverComputation,
+      verbose = false)
+
+    val ollComputation = new OLLComputation(n,
+      neverMutateZeroBits = true, //cmd.getBoolean("never-mutate-zero-bits"),
+      includeBestMutantInComparison = true, //cmd.getBoolean("include-best-mutant"),
+      ignoreCrossoverParentDuplicates = true, //cmd.getBoolean("ignore-crossover-parent-duplicates"),
+      crossoverComputation = crossoverComputation)
+
+    val popSizes = lambdas.map(v => math.round(v).toInt)
+    val idx1 = lambdas.length - 1
+    val idx2 = lambdas.length - 2
+
+    val minLambda1 = 20
+    val maxLambda1 = 30
+    val minLambda2 = 12
+    val maxLambda2 = 22
+
+    case class Result(lambda1: Double, lambda1Idx: Int, lambda2: Double, lambda2Idx: Int, result: Double)
+
+    val tasks = new java.util.ArrayList[Callable[Result]]
+    val lambdaCount = 100
+    for (lambda1Idx <- 0 until lambdaCount; lambda2Idx <- 0 until lambdaCount) {
+      tasks.add(() => {
+        val lambda1 = minLambda1 + (lambda1Idx + 0.5) / lambdaCount * (maxLambda1 - minLambda1)
+        val lambda2 = minLambda2 + (lambda2Idx + 0.5) / lambdaCount * (maxLambda2 - minLambda2)
+        val myLambdas = lambdas.clone()
+        val myPopSizes = popSizes.clone()
+        myLambdas(idx1) = lambda1
+        myPopSizes(idx1) = math.round(lambda1).toInt
+        myLambdas(idx2) = lambda2
+        myPopSizes(idx2) = math.round(lambda2).toInt
+        val result = RunGivenLambdas.run(n, bins, myLambdas, myPopSizes, ollComputation)
+        println(s"lambda1 = $lambda1, lambda2 = $lambda2, result = $result")
+        Result(lambda1, lambda1Idx, lambda2, lambda2Idx, result)
+      })
+    }
+
+    val pool = new ScheduledThreadPoolExecutor(Runtime.getRuntime.availableProcessors())
+    val results = pool.invokeAll(tasks).asScala.map(_.get())
+    pool.shutdown()
+
+    val resultMin = results.minBy(_.result).result
+    val resultMax = results.maxBy(_.result).result
+    println(s"min = $resultMin, max = $resultMax")
+
+    val image = new BufferedImage(lambdaCount, lambdaCount, BufferedImage.TYPE_INT_RGB)
+    for (Result(_, lambda1Idx, _, lambda2Idx, result) <- results) {
+      val normalized = (result - resultMin) / (resultMax - resultMin)
+      image.setRGB(lambda1Idx, lambdaCount - lambda2Idx - 1, Viridis(normalized))
+    }
+    Using.resource(Files.newOutputStream(Paths.get("two-lambdas.png")))(stream => ImageIO.write(image, "png", stream))
+  }
+
   private def varyingLastLambda(): Unit = {
     val n = 500
     val bins = RunGivenLambdas.defaultBins(n)
@@ -72,6 +138,7 @@ object DemoPlots {
   }
 
   def main(args: Array[String]): Unit = {
-    varyingLastLambda()
+    //varyingLastLambda()
+    varyingLastTwoLambdas()
   }
 }
